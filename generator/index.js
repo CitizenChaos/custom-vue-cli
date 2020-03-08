1786708bfcbac54a334cd0df92720764933c6097
Wang Yang <785064576@qq.com>
2020-03-08 17:40:46 +0800

    根据模板类型，渲染不同的文件

--- generator/index.js
+++ generator/index.js
@@ -5,11 +5,27 @@ module.exports = (api, options, rootOptions) => {
       'build-test': 'vue-cli-service build --mode test'
     },
     dependencies: {
-      'axios': '^0.19.0'
+      axios: '^0.19.0'
     },
     devDependencies: {
       'compression-webpack-plugin': '^3.0.0'
     }
+  })
+
+  // 复制并用 ejs 渲染 `./template` 内所有的文件
+  api.render('../template')
+
+  api.render((files, render) => {
+    delete files['src/views/About.vue']
+    delete files['src/components/HelloWorld.vue']
+    if (options.module === 'PC') {
+      files['src/main.js'] = files['src/main.pc.js']
+      delete files['src/utils/rem.js']
+    } else if (options.module === 'mobile') {
+      files['src/main.js'] = files['src/main.mobile.js']
+    }
+    delete files['src/main.pc.js']
+    delete files['src/main.mobile.js']
   })
 
   if (options.module === 'PC') {
@@ -21,11 +37,8 @@ module.exports = (api, options, rootOptions) => {
   } else if (options.module === 'mobile') {
     api.extendPackage({
       dependencies: {
-        'vant': '^2.4.7'
+        vant: '^2.4.7'
       }
     })
   }
-
-  // 复制并用 ejs 渲染 `./template` 内所有的文件
-  api.render('../template')
 }
